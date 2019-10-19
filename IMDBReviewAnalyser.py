0006d59878a8ec54ea742287ec36a47cf05f5ea7
Graeme Hosford <graemehosford@gmail.com>
2019-10-19 15:21:45 +0100

    Updated up to part 4, might sort of work but unsure yet

--- IMDBReviewAnalyser.py
+++ IMDBReviewAnalyser.py
@@ -1,17 +1,14 @@
 # Name: Graeme Hosford
 # Student ID: R00147327
 
-import numpy as np
-import pandas as pd
-import re
-
-from itertools import chain
 from collections import Counter
 
-data = pd.read_excel("movie_reviews.xlsx")
+import pandas as pd
 
 
 def split_data():
+    data = pd.read_excel("movie_reviews.xlsx")
+
     is_train_data = data["Split"] == "train"
     is_test_data = data["Split"] == "test"
 
@@ -30,20 +27,21 @@ def split_data():
     return review_training_data, labels_training_data, review_test_data, labels_test_data
 
 
-def count_training_words(training_reviews: pd.Series, min_word_length, min_word_occurance):
-    training_reviews.replace(to_replace="[^A-Za-z0-9\s]+", value="", regex=True, inplace=True)
-    training_reviews = training_reviews.apply(lambda x: x.lower())
+def clean_data(review_data: pd.Series) -> pd.Series:
+    review_data.replace(to_replace="[^A-Za-z0-9\s]+", value="", regex=True, inplace=True)
+    return review_data.apply(lambda x: x.lower())
 
+
+def count_training_words(training_reviews: pd.Series, min_word_length, min_word_occurance) -> set:
     word_freq_dict = dict()
 
     for review in training_reviews:
-        words = review.lower().split()
-        for word in words:
+        for word in review.split():
             if len(word) >= min_word_length:
                 if word in word_freq_dict.keys():
                     word_freq_dict[word] += 1
                 else:
-                    word_freq_dict[word] = 0
+                    word_freq_dict[word] = 1
 
     new_dict = dict()
 
@@ -51,34 +49,58 @@ def count_training_words(training_reviews: pd.Series, min_word_length, min_word_
         if value >= min_word_occurance:
             new_dict[key] = value
 
-    return training_reviews, list(new_dict.keys())
+    return set(new_dict.keys())
 
 
-def count_word_in_review_frequency(review_list,  word_list):
-    word_freq_dict = dict()
+def count_word_in_review_frequency(review_list: pd.Series, word_set: set) -> Counter:
+    word_freq_dict = Counter(word_set)
+
+    for k in word_freq_dict.keys():
+        word_freq_dict[k] = 0
 
     for review in review_list:
-        for word in review.split():
-            if word in word_list:
-                if word in word_freq_dict.keys():
-                    word_freq_dict[word] += 1
-                else:
-                    word_freq_dict[word] = 0
+        for word in word_set:
+            if word in review:
+                word_freq_dict[word] += 1
 
     return word_freq_dict
 
 
+def calculate_likelihood(word_freq: Counter, data_set: pd.Series) -> dict:
+    word_likelihood = dict()
+    smoothing = 1
+
+    for word in word_freq.keys():
+        word_likelihood[word] = (word_freq[word] + smoothing) / (len(data_set) + smoothing)
+
+    return word_likelihood
+
+
+def calculate_priors(full_dataset: pd.Series, subset: pd.Series):
+    return len(subset) / len(full_dataset)
+
+
 def main():
     review_train_data, label_train_data, review_test_data, label_test_data = split_data()
-    review_train_data, word_list = count_training_words(review_train_data, 3, 400)
 
-    word_set = set(word_list)
+    review_train_data = clean_data(review_train_data)
+
+    word_set = count_training_words(review_train_data, 4, 100)
 
     positive_training_reviews = review_train_data[label_train_data == "positive"]
     negative_training_reviews = review_train_data[label_train_data == "negative"]
 
     positive_word_freq_dict = count_word_in_review_frequency(positive_training_reviews, word_set)
     negative_word_freq_dict = count_word_in_review_frequency(negative_training_reviews, word_set)
+
+    positive_likelihood = calculate_likelihood(positive_word_freq_dict, positive_training_reviews)
+    negative_likelihood = calculate_likelihood(negative_word_freq_dict, negative_training_reviews)
+
+    positive_prior = calculate_priors(review_train_data, positive_training_reviews)
+    negative_prior = calculate_priors(review_train_data, negative_training_reviews)
+
+    print(positive_prior)
+    print(negative_prior)
 
 
 main()
